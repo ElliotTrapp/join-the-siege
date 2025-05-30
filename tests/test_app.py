4eebd1f1356e0c42face5efa52debe51963a6b1b
Elliot <20442793+ElliotTrapp@users.noreply.github.com>
2025-05-30 11:32:30 -0700

    moving stopwords around in test to hopfully fix it on github

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,16 +2,12 @@ from io import BytesIO
 
 import pytest
 from src.app import app
-import nltk
-nltk.download("wordnet")
-nltk.download("stopwords")
 
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
-
 
 def test_no_file_in_request(client):
     response = client.post('/classify_file')
